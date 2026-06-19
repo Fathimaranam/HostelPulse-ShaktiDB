eb470ad3a8125ac1bcc3c44c0e07ffe5664c0b50
Fathima Rana <240960@tkmce.ac.in>
2026-06-19 04:11:13 -0700

    Implemented Add Student feature

--- src/app.py
+++ src/app.py
@@ -14,13 +14,32 @@ def view_students():
     cur = conn.cursor()
 
     cur.execute("SELECT * FROM students")
-
     students = cur.fetchall()
 
     print("\n===== Students =====")
-
     for student in students:
         print(student)
+
+    cur.close()
+    conn.close()
+
+def add_student():
+    name = input("Enter Name: ")
+    hostel = input("Enter Hostel: ")
+    room_no = input("Enter Room Number: ")
+    phone = input("Enter Phone Number: ")
+
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO students(name, hostel, room_no, phone)
+        VALUES (%s, %s, %s, %s)
+    """, (name, hostel, room_no, phone))
+
+    conn.commit()
+
+    print("Student added successfully!")
 
     cur.close()
     conn.close()
@@ -29,7 +48,8 @@ while True:
 
     print("\n===== HostelPulse =====")
     print("1. View Students")
-    print("2. Exit")
+    print("2. Add Student")
+    print("3. Exit")
 
     choice = input("Enter choice: ")
 
@@ -37,6 +57,9 @@ while True:
         view_students()
 
     elif choice == "2":
+        add_student()
+
+    elif choice == "3":
         print("Goodbye!")
         break
 
